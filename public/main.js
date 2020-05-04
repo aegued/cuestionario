995d3af141863920e4390d136a89c9b8ae70698f
alfredo <egued89@gmail.com>
2020-05-05 00:18:58 +0200

    Frontend Questionnaire OK

---
+++ public/main.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+
+    $('#questions-tab li:first-child a').addClass('active');
+    $('#questions-tabContent .tab-pane:first-child').addClass('active');
+    $('.tab-pane:first-child .prev-step').addClass('d-none');
+
+    //Wizard
+    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+        let $target = $(e.target);
+        if ($target.parent().hasClass('disabled')) {
+            return false;
+        }
+    });
+
+    $(".next-step").click(function (e) {
+        let $active = $('.nav-pills li > a.active');
+        $active.parent().next().find('.nav-link').removeClass('disabled');
+
+        let form = $(this).parent().parent();
+        let inputAnswer = form.find('#answer');
+        let feedback = form.find('.invalid-feedback');
+
+        resetMessage(inputAnswer, feedback);
+
+        $.ajax({
+            url: form.attr('action'),
+            method: 'POST',
+            data: form.serialize(),
+            success: function (response) {
+                if (response.check) {
+                    successMessage(inputAnswer, feedback);
+
+                    setTimeout(function () {
+                        nextTab($active);
+                    }, 1000);
+
+                } else {
+                    errorMessage(inputAnswer, feedback);
+                }
+            }
+        });
+    });
+
+    $(".prev-step").click(function (e) {
+        var $active = $('.nav-pills li > a.active');
+        prevTab($active);
+    });
+
+    function nextTab(elem) {
+        $(elem).parent().next().find('a[data-toggle="pill"]').click();
+    }
+    function prevTab(elem) {
+        $(elem).parent().prev().find('a[data-toggle="pill"]').click();
+    }
+    function resetMessage(input, feedback) {
+        input.removeClass('is-invalid');
+        input.removeClass('is-valid');
+        feedback.addClass('d-none');
+        input.parent().find('.alert').remove();
+    }
+    function errorMessage(input, feedback) {
+        input.val('');
+        input.addClass('is-invalid');
+        feedback.html('La respuesta es incorrecta. Vuelva a intentarlo.');
+        feedback.removeClass('d-none');
+    }
+    function successMessage(input, feedback) {
+        input.addClass('is-valid');
+        input.removeClass('is-invalid');
+        input.parent().append(
+            '<div class="alert alert-success mt-3">Respuesta correcta</div>'
+        );
+    }
+});
--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,43 +1,68 @@
 @extends('front.layouts.app')
 
 @section('content')
-    <form method="POST" id="wizard-form">
-        @csrf
-        @php $count = 0; @endphp
-        @foreach($questions as $question)
-        <h3>{{ $questionnaire->name }}</h3>
+    <div class="row justify-content-center mt-0">
+        <div class="col-11 col-sm-9 col-md-7 col-lg-6 text-center p-0 mt-3 mb-2">
+            <div class="card px-5 py-3 mt-3 mb-3">
+                <div class="row">
+                    <div class="col-md-12">
 
-        <fieldset>
-            <h2>{{ $question->question }}</h2>
+                        <ul class="nav nav-pills justify-content-center mb-3" id="questions-tab" role="tablist">
+                            @php $count = 0; @endphp
+                            @foreach($questions as $question)
+                                @php $count++; @endphp
+                                <li class="nav-item">
+                                    <a class="nav-link" id="questions-tab-1" data-toggle="pill" href="#questions-content-{{ $question->id }}" role="tab">{{ $count }}</a>
+                                </li>
+                            @endforeach
+                                <li class="nav-item">
+                                    <a class="nav-link" id="questions-tab-1" data-toggle="pill" href="#finish-content" role="tab">{{ $count+1 }}</a>
+                                </li>
+                        </ul>
 
-            <div class="form-group" id="step-@php echo $count; @endphp">
-                <input
-                    type="text"
-                    name="answer"
-                    id="answer"
-                    class="form-control"
-                    placeholder="Escribe tu respuesta ... "
-                    data-url="{{ route('questions.check',$question->id) }}"
-                />
-                <span id="exampleInputEmail1-error" class="error invalid-feedback d-none"></span>
+                        <div class="tab-content" id="questions-tabContent">
+                            @foreach($questions as $question)
+                                <div class="tab-pane fade show" id="questions-content-{{ $question->id }}" role="tabpanel">
+                                    <h4 class="mb-5">{{ $question->question }}</h4>
+
+                                    <form action="{{ route('questions.check', $question->id) }}" method="POST" class="form-question">
+                                        @csrf
+                                        <div class="form-group answer-fields">
+                                            <input type="text" class="form-control" name="answer" id="answer" placeholder="Escribe la respuesta ...">
+                                            <span id="exampleInputEmail1-error" class="error invalid-feedback d-none"></span>
+                                        </div>
+
+                                        <div class="form-group text-center">
+                                            <button type="button" class="btn btn-primary btn-sm prev-step">Anterior</button>
+                                            <button type="button" class="btn btn-primary btn-sm next-step">Siguiente</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            @endforeach
+                                <div class="tab-pane fade" id="finish-content" role="tabpanel">
+                                    <div class="alert alert-success text-center">
+                                        Enhorabuena has finalizado el test
+                                    </div>
+                                    <form>
+                                        <div class="form-group text-center">
+                                            <button type="button" class="btn btn-primary btn-sm prev-step">Anterior</button>
+                                        </div>
+
+                                    </form>
+                                </div>
+                        </div>
+
+                    </div>
+                </div>
             </div>
-        </fieldset>
-            @php $count++; @endphp
-        @endforeach
-
-    </form>
+        </div>
+    </div>
 @endsection
 
 @section('css')
-    <!-- Font Icon -->
-    <link rel="stylesheet" href="{{ asset('plugins/colorlib-wizard/fonts/material-icon/css/material-design-iconic-font.min.css') }}">
-    <!-- Wizard css -->
-    <link rel="stylesheet" href="{{ asset('plugins/colorlib-wizard/css/style.css') }}">
+
 @endsection
 
 @section('js')
-    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/jquery.validate.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/additional-methods.min.js"></script>
-    <script src="{{ asset('plugins/jquery-steps/jquery.steps.min.js') }}"></script>
-    <script src="{{'plugins/colorlib-wizard/js/main.js'}}"></script>
+    <script src="{{ asset('main.js') }}"></script>
 @endsection
